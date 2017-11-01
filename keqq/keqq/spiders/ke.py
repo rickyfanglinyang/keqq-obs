7ac159710c3595c03a6d61ed01e773f49e30d5eb
Fanglin <yfl0406@gmail.com>
2017-11-02 06:51:18 +0800

    dev creation

--- keqq/keqq/spiders/ke.py
+++ keqq/keqq/spiders/ke.py
@@ -37,75 +37,31 @@ class KeSpider(scrapy.Spider):
         sold_by  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/div[@class='item-line item-line--middle']/span[@class='item-source']/a[@class='item-source-link']/text()").extract()
 
         list_courses = []
-        
-        # #Get count for selectors 
-        # record_num = len(response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']"))
-        # s_record_num = len(response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/div[@class='item-line item-line--middle']/span[@class='line-cell item-user']"))
-        # p_record_num = len(response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/div[@class='item-line item-line--bottom']/span[@class='line-cell item-price']"))
 
+        for course in response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']"):
+            course_name = course.xpath("./h4[@class='item-tt']/a[@class='item-tt-link']/text()").extract_first()
+            sold_count  = course.xpath("./div[@class='item-line item-line--middle']/span[@class='line-cell item-user']/text()").extract_first()
+            price  = course.xpath("./div[@class='item-line item-line--bottom']/span[@class='line-cell item-price']/text()").extract_first()
+            sold_by  = course.xpath("./div[@class='item-line item-line--middle']/span[@class='item-source']/a[@class='item-source-link']/text()").extract_first()
 
-        # print("Number of selectors ##", record_num)
+            print("course_name ##: ", course_name)
+            print("sold_count ##: ", sold_count)
+            print("price ##: ", price)
+            print("sold_by ##: ", sold_by)
 
-        # for i  in range(1,record_num + 1):
-        #     course_name  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/h4[@class='item-tt']/a[@class='item-tt-link']/text()")[i].extract()
-        #     if s_record_num > 0:
-        #         sold_count  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/div[@class='item-line item-line--middle']/span[@class='line-cell item-user']/text()")[i].extract()
-        #     else:
-        #         sold_count = 0
-            
-        #     if p_record_num > 0:
-        #         price  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/div[@class='item-line item-line--bottom']/span[@class='line-cell item-price']/text()")[i].extract()
-        #     else:
-        #         price = 0
+            a_course = []
+            a_course.append(course_name)
+            a_course.append(sold_count)
+            a_course.append(price)
+            a_course.append(sold_by)
 
-        #     sold_by  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/div[@class='item-line item-line--middle']/span[@class='item-source']/a[@class='item-source-link']/text()")[i].extract()
-            
-        #     a_course = []
-        #     a_course.append(course_name)
-        #     a_course.append(sold_count)
-        #     a_course.append(price)
-        #     a_course.append(sold_by)
+            logging.log(logging.WARNING, "####" * 100)
+            logging.log(logging.WARNING, a_course)
+            logging.log(logging.WARNING, "----" * 100)
 
-        #     logging.log(logging.WARNING, "####" * 200)
-        #     logging.log(logging.WARNING, a_course)
-        #     logging.log(logging.WARNING, "----" * 200)
+            list_courses.append(a_course)
 
-        #     list_courses.append(a_course)
+        logging.log(logging.WARNING, "###All Courses###")
+        logging.log(logging.WARNING,list_courses)      
 
-
-
-        # logging.log(logging.WARNING, "###All Courses###")
-        # logging.log(WARNING,list_courses)      
-
-
-
-
-
-
-            # logging.log(logging.WARNING, "####" * 200)
-            # logging.log(logging.WARNING, course_name)
-            # logging.log(logging.WARNING, sold_count)
-            # logging.log(logging.WARNING, price)
-            # logging.log(logging.WARNING, sold_by)
-            # logging.log(logging.WARNING, "----" * 200)
-
-            # print(course_name)
-            # print(sold_count)
-            # print(price)
-            # print(sold_by)
-
-
-        # print(list_courses)
-        result = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']")
-        doc = etree.fromstring(str(result))        
-
-
-
-
-
-# atags = doc.xpath('//a')
-# for a in atags:
-#     btags = a.xpath('b')
-#     for b in btags:
-#             print b
 
